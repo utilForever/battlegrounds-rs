8081947aed2302ece69f54bdb7a4a5c9b22111d3
Chris Ohk <utilforever@gmail.com>
2021-12-05 21:40:08 +0900

    feat(parsing): Add code to get values of the card

--- src/loaders/card_loader.rs
+++ src/loaders/card_loader.rs
@@ -1,8 +1,9 @@
-use crate::enums::card_set::CardSet;
+use crate::enums::{card_set::CardSet, card_type::CardType, game_tag::GameTag, race::Race};
 
 use serde_json::Value;
 use specs::prelude::*;
 
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 use std::path::{Display, Path, PathBuf};
@@ -50,6 +51,68 @@ impl CardLoader {
 
             if card_set == CardSet::LETTUCE as i32 {
                 continue;
+            }
+
+            let id = card["id"].as_str();
+            let dbf_id = if card["dbfID"].is_null() {
+                0
+            } else {
+                FromStr::from_str(card["dbfID"].as_str().unwrap()).unwrap()
+            };
+            let normal_dbf_id = if card["battlegroundsNormalDbfId"].is_null() {
+                0
+            } else {
+                FromStr::from_str(card["battlegroundsNormalDbfId"].as_str().unwrap()).unwrap()
+            };
+            let premium_dbf_id = if card["battlegroundsPremiumDbfId"].is_null() {
+                0
+            } else {
+                FromStr::from_str(card["battlegroundsPremiumDbfId"].as_str().unwrap()).unwrap()
+            };
+            let is_battlegrounds_hero = !card["battlegroundsHero"].is_null();
+
+            let name = if card["name"].is_null() {
+                ""
+            } else {
+                card["name"].as_str().unwrap()
+            };
+            let text = if card["text"].is_null() {
+                ""
+            } else {
+                card["text"].as_str().unwrap()
+            };
+
+            let card_type = if card["type"].is_null() {
+                CardType::INVALID
+            } else {
+                CardType::from_str(card["type"].as_str().unwrap()).unwrap()
+            };
+            let race = if card["race"].is_null() {
+                Race::INVALID
+            } else {
+                Race::from_str(card["race"].as_str().unwrap()).unwrap()
+            };
+
+            let tech_level = if card["techLevel"].is_null() {
+                0
+            } else {
+                FromStr::from_str(card["techLevel"].as_str().unwrap()).unwrap()
+            };
+            let attack = if card["attack"].is_null() {
+                0
+            } else {
+                FromStr::from_str(card["attack"].as_str().unwrap()).unwrap()
+            };
+            let health = if card["health"].is_null() {
+                0
+            } else {
+                FromStr::from_str(card["health"].as_str().unwrap()).unwrap()
+            };
+
+            let mut game_tags = HashMap::new();
+            for mechanic in card["mechanics"].as_array().unwrap().iter() {
+                let game_tag = GameTag::from_str(mechanic.as_str().unwrap()).unwrap();
+                game_tags.insert(game_tag, 1);
             }
         }
     }
