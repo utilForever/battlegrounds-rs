0ee27c99fde4a99ad660f2870d8f8268bdbfdfa8
Ray <vecum0814@naver.com>
2022-08-02 00:06:02 +0900

    Deserealise HearthStone JSON file editied v1

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod components;
 pub mod enums;
 pub mod loaders;
+
--- src/loaders/json_reader.rs
+++ src/loaders/json_reader.rs
@@ -1,35 +1,10 @@
 use std::path::Path;
 use std::fs::File;
 
-use serde::{Deserialize};
+#[cfg(feature = "serde")]
+extern crate serde;
 
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct User {
-    username: String,
-    first_name: String,
-    last_name: String,
-    email: String
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-struct Person {
-    first_name: String,
-    last_name: String,
-    age: u8,
-    address: Address,
-    phone_numbers: Vec<String>,
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-struct Address {
-    street: String,
-    city: String,
-    country: String,
-}
+use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -56,29 +31,27 @@ struct Card {
     r#type: String,
 }
 
-
-
-
 fn main() {
-    let json_file_path = Path::new("/Users/raylee/Rust/json_reader/src/cards_kor.json");
+    let path = Path::new("/Users/raylee/Rust/json_reader/src/cards_kor.json");
     let file = File::open(json_file_path).expect("file not found");
     
-    let mut battleground_cards = Vec::new();
+    let mut cards = Vec::new();
 
 
-    let cards:Vec<Card> = serde_json::from_reader(file).expect("JSON was not well-formatted");
-    for card in cards {
+    let raw_cards:Vec<Card> = serde_json::from_reader(file).expect("Invalid format");
+    
+    for card in raw_cards.iter() {
         if card.battlegrounds_premium_dbf_id.is_some() {
-            battleground_cards.push(card)
+            cards.push(card)
         }
         //println!("dbfId: {}", card.dbf_id)
     }
 
-    for card in &battleground_cards {
+   // for card in &battleground_cards {
  
-        println!("Card Name: {}", card.name)
-    }
-    println!("{}", battleground_cards.len())
+   //     println!("Card Name: {}", card.name)
+   // }
+    println!("{}", cards.len())
 
 }
 
